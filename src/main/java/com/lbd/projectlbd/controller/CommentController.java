5ae6c99751c2036842c0c120e666c5f240a7bda4
wiktfic226 <wiktoriafica00@gmail.com>
2022-08-10 12:37:38 +0200

    feat: update comment controller methods

--- src/main/java/com/lbd/projectlbd/controller/CommentController.java
+++ src/main/java/com/lbd/projectlbd/controller/CommentController.java
@@ -1,6 +1,5 @@
 package com.lbd.projectlbd.controller;
 
-
 import com.lbd.projectlbd.apiresponse.StandardResponse;
 import com.lbd.projectlbd.dto.CommentDto;
 import com.lbd.projectlbd.service.comment.CommentServiceImpl;
@@ -10,40 +9,49 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/comment")
 @RequiredArgsConstructor
 public class CommentController {
 
-    private final CommentServiceImpl service;
+    private final CommentServiceImpl commentService;
 
     @PostMapping
     public ResponseEntity<StandardResponse> addComment(@RequestBody CommentDto comment) {
-        service.add(comment);
+        commentService.add(comment);
         return new StandardResponse(HttpStatus.OK, "Comment added").buildResponseEntity();
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<StandardResponse> deleteComment(@PathVariable Long id) {
-        service.delete(id);
+        commentService.delete(id);
         return new StandardResponse(HttpStatus.OK, "Comment deleted").buildResponseEntity();
     }
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<StandardResponse> updateComment(@PathVariable Long id, @RequestBody CommentDto commentDto) {
-        service.update(id, commentDto);
+        commentService.update(id, commentDto);
         return new StandardResponse(HttpStatus.OK, "Comment updated").buildResponseEntity();
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<CommentDto> getCommentById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(service.findById(id));
+        return ResponseEntity.ok().body(commentService.findById(id));
     }
 
     @GetMapping("delegation/{delegationId}")
     public ResponseEntity<List<CommentDto>> getCommentByDelegationId(@PathVariable Long delegationId) {
-        return ResponseEntity.ok().body(service.getAllByDelegationId(delegationId));
+        return ResponseEntity.ok().body(commentService.getAllByDelegationId(delegationId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentDto>> getComments() {
+        return ResponseEntity.ok().body(commentService.getAllComments());
+    }
+
+    @GetMapping("parent/{parentId}")
+    public ResponseEntity<List<CommentDto>> getCommentByParentId(@PathVariable Long parentId) {
+        return ResponseEntity.ok().body(commentService.getAllByUpComment(parentId));
     }
 }
